067d276f076683e94d0de9eb8afdbd889edc6452
Jesusalexander <brpoper@gmail.com>
2017-08-28 22:29:09 +0800

    getAPIUrl without host

--- test/API.spec.js
+++ test/API.spec.js
@@ -27,4 +27,19 @@ describe('API', () => {
     )
     expect(url).to.be.contains('api=SYNO.DownloadStation.Task', 'DownloadStation/', 'task.cgi', '127.0.0.1')
   })
+  it('get api Url without Host', () => {
+    const url = api.getAPIUrl(
+      {
+        cgi: 'task',
+        name: 'DownloadStation',
+        api: 'SYNO.DownloadStation.Task',
+        version: 1,
+        _sid: 'XhO7z.Oq6N01.1690O6N720701'
+      },
+      true
+    )
+    console.log(url)
+    expect(url).to.be.contains('api=SYNO.DownloadStation.Task', 'DownloadStation/', 'task.cgi',)
+    expect(url).not.contains('127.0.0.1')
+  })
 })
